e075b9643bb8255c68f50de18c3ccfe9c53a8e99
babarali581 <cloudnosys.babar@gmail.com>
2020-07-22 22:45:26 +0500

    Updates

---
+++ precache-manifest.6a9ed537bba88357cc4a2cf580fa871f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77be98956867dd66521074b06372b9b2",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "047e818ce3244e68c81b",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "047e818ce3244e68c81b",
+    "url": "/bmi-calculator/static/js/main.3b77b15d.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
